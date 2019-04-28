5906ee5d56cb707da5070fb0a1e4ce8efcd3c1e8
Naief Jobsen <gusjobna@student.gu.se>
2019-04-28 22:31:12 +0200

    [WIP] Finished the faq-advice component. All content is ready

--- src/components/sub_components/faq-cards/faq-advice.vue
+++ src/components/sub_components/faq-cards/faq-advice.vue
@@ -10,8 +10,35 @@
             <h3>Tips & Tricks</h3>
           </b-card-text>
           <b-card-text>{{ text1 }}</b-card-text>
-          <hr class="hr"> 
+          <hr class="hr">
           <b-card-text>{{ text2 }}</b-card-text>
+          <hr class="hr">
+          <b-card-text> 
+              Delegating tasks is important. Explore everyone's unique capabilities: Divide and conquer!
+          Try to assign different responsibilities, for instance:
+          
+          <li>Coding: Front-end, back-end, ripping data from websites</li>
+          <li> Pitching the idea (slides, catch phrases), designing the interface</li> 
+          Dev. 
+          <li> Using tools (git, APIs, etc.) properly, track time and goals, talk with organization</li>              
+          </b-card-text>
+          <hr class="hr">
+        
+          <li>There are lots of tips on winning and enjoying Hackathon. Here is 
+              <a href="https://goo.gl/goXESf" target="_blank"> 
+              <hlt>one, </hlt>
+              </a> this is
+              <a href="https://hackathon.guide/" target="_blank">
+              <hlt>another</hlt>
+              </a>, or you
+              <a href="http://lmgtfy.com/?q=how+to+win+a+hackathon" target="_blank">
+              <hlt>simply do this</hlt>.
+              </a>
+              <p></p>
+              <p>Above all: Be fun, creative and learn lots of new stuff (technical and personal).</p>
+              </li>
+        
+
 
         </b-card-body>
       </b-collapse>
@@ -25,15 +52,16 @@ export default {
   name: "faqadvice",
   data() {
     return {
-      text1: 
-      `There is no I in TEAM. So, remember to build a 
+      text1: `There is no I in TEAM. So, remember to build a 
         good team with a variety of expertise so everyone 
         can share and learn from one another.
         Being friendly (not FRIENDS) is essencial for 
         collaboration in a Hackathon. Especially among 
         different teams. Remember, everyone is in the same boat: sink or swim.`,
-        text2: 
-      `FUCK FUUUUCK`
+      text2: 
+      `You can come with a team of yours or meet people and join/form 
+        teams in our brainstorming evening. Remember that they have to be CSE students.`,
+    
     };
   }
 };
@@ -41,6 +69,5 @@ export default {
 
 
 <style scoped>
-    @import "../../../../src/assets/style/line-style.css";
-
+@import "../../../../src/assets/style/line-style.css";
 </style>
