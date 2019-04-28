738b39a6160613f9f9fea7950392623b2e54e99a
Naief Jobsen <gusjobna@student.gu.se>
2019-04-28 21:58:52 +0200

    [WIP] Added a new style sheet for the line. Added content for faq-advice

--- src/components/sub_components/faq-cards/faq-advice.vue
+++ src/components/sub_components/faq-cards/faq-advice.vue
@@ -1,44 +1,46 @@
  <template>
- 
- <div role="tablist">
+  <div role="tablist">
     <b-card no-body class="mb-1">
       <b-card-header header-tag="header" class="p-1" role="tab">
-        <b-button block href="#" v-b-toggle.accordion-1 variant="info">Accordion 1</b-button>
+        <b-button block href="#" v-b-toggle.accordion-1 variant="dark">Is there any advice?</b-button>
       </b-card-header>
       <b-collapse id="accordion-1" visible accordion="my-accordion" role="tabpanel">
         <b-card-body>
-          <b-card-text>I start opened because <code>visible</code> is <code>true</code></b-card-text>
-          <b-card-text>{{ text }}</b-card-text>
+          <b-card-text>
+            <h3>Tips & Tricks</h3>
+          </b-card-text>
+          <b-card-text>{{ text1 }}</b-card-text>
+          <hr class="hr"> 
+          <b-card-text>{{ text2 }}</b-card-text>
+
         </b-card-body>
       </b-collapse>
     </b-card>
- </div>
-
+  </div>
 </template>
 <script>
-import Collapse from 'bootstrap-vue/es/components/collapse'
+import Collapse from "bootstrap-vue/es/components/collapse";
 
 export default {
   name: "faqadvice",
-    data() {
-      return {
-        text: `
-          Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry
-          richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor
-          brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon
-          tempor, sunt aliqua put a bird on it squid single-origin coffee nulla
-          assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore
-          wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher
-          vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic
-          synth nesciunt you probably haven't heard of them accusamus labore VHS.
-        `
-      }
-    }
+  data() {
+    return {
+      text1: 
+      `There is no I in TEAM. So, remember to build a 
+        good team with a variety of expertise so everyone 
+        can share and learn from one another.
+        Being friendly (not FRIENDS) is essencial for 
+        collaboration in a Hackathon. Especially among 
+        different teams. Remember, everyone is in the same boat: sink or swim.`,
+        text2: 
+      `FUCK FUUUUCK`
+    };
+  }
 };
 </script>
 
 
 <style scoped>
-    @import "../../../../src/assets/style/temp-style.css";
+    @import "../../../../src/assets/style/line-style.css";
 
 </style>
